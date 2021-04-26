080e8a6c2a35dc30b3981eade3d163a44c2801c1
sjcco <73748496+sjcco@users.noreply.github.com>
2021-04-26 18:37:19 -0500

    Add layout and markup plus devise user model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   # GET /posts/1 or /posts/1.json
@@ -64,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:title, :body)
     end
 end
